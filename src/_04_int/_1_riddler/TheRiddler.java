3369002ed08fd9540a9d18117fd85adbc6fdbe79
student <student@ip-172-31-58-47.us-west-2.compute.internal>
2021-08-31 19:52:16 -0700

    Finished the riddler, Robot in space, and zombie eyes. Started rocket ship.

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -15,14 +15,31 @@ public class TheRiddler {
 		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
 		String Q1 = JOptionPane.showInputDialog("What question can you never answer yes to?");
-		String Q1 = JOptionPane.showInputDialog("What question can you never answer yes to?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-		if (Q1.equals ("Are you asleep yet?")) {
+		if (Q1.equals ("are you asleep")) {
 			JOptionPane.showMessageDialog(null, "You Got It!");
+			score ++;
 		}
 		else {
 			JOptionPane.showMessageDialog(null, "Wrong!");
 		}
+		String Q2 = JOptionPane.showInputDialog("What month of the year has 28 days?");
+		if (Q2.equals ("all of them")) {
+			JOptionPane.showMessageDialog(null, "You Got It!");
+			score ++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong!");
+		}
+		String Q3 = JOptionPane.showInputDialog("What can you break, even if you never pick it up or touch it?");
+		if (Q3.equals ("a promise")) {
+			JOptionPane.showMessageDialog(null, "You Got It!");
+			score ++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong!");
+		}
+		JOptionPane.showMessageDialog(null, "your score is "+score);
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,16 @@
-
+PImage face;
 void setup() {
+size (500, 500);
+face = loadImage("Zombie.jpg");
+face.resize(500,500);
 
 }
 void draw() {
-
+background(face);
+fill (mouseX, mouseY, mouseX - mouseY);
+ellipse(225,250,120,120);
+ellipse(420,260,100,140);
+fill (#030303);
+ellipse(225,250,30,30);
+ellipse(420,260,30,30);
 }
